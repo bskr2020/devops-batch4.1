4f9e5ee2c2117607da6bf80ed4720675842c7b31
bskr2020 <bskr.2020@gmail.com>
2021-07-07 06:36:24 +0530

    Pushing new project to github

---
+++ src/student.java
@@ -0,0 +1,7 @@
+class Student{
+    public static void main(String[] args) {
+        System.out.println("This is my secound project");
+        System.out.println("This is my devops project");
+        System.out.println("This is my devops-github project");
+    }
+}
