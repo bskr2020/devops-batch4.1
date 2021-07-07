95c430e130297a900c0f72c2de83e03867ba68bb
bskr2020 <bskr.2020@gmail.com>
2021-07-07 06:50:39 +0530

    new method added.

---
+++ src/department.java
@@ -0,0 +1,14 @@
+class department{
+    public void m1(){
+        System.out.println("My first method");
+        System.out.println("My second method");
+        System.out.println("My third method");
+
+    }
+    public void m2(){
+        System.out.println("My first method");
+        System.out.println("My second method");
+        System.out.println("My third method");
+
+    }
+}
